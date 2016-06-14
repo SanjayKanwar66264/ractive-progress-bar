b0914e781e4bde9cfcc8194e29736e0f46679971
Sanjay Kanwar <sanj.kan123@gmail.com>
2016-06-14 10:54:17 +1000

    fixed updating the progress bar value

--- js/progress_bar.js
+++ js/progress_bar.js
@@ -15,24 +15,22 @@ var ractive = new Ractive({
 function changeProgressBarValue() {
     var e = document.getElementById("progessBarType");
     var strUser = e.options[e.selectedIndex].value;
-    debugger;
     switch (event.target.innerHTML) {
         case"MinusTwentyFive":
             document.getElementById(strUser).value -= 25;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value;
+            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
             break;
         case"MinusTen":
             document.getElementById(strUser).value -= 10;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value;
+            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
             break;
         case"PlusTen":
             document.getElementById(strUser).value += 10;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value;
+            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
             break;
         case"PlusTwentyFive":
             document.getElementById(strUser).value += 25;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value;
-
+            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
             break;
         default :
             break;
