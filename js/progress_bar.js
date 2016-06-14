5080ae4a6b4a9bb0ffa2e92a99b570f322142d59
Sanjay Kanwar <sanj.kan123@gmail.com>
2016-06-14 13:36:49 +1000

    fixed value increasing to more then 100

--- js/progress_bar.js
+++ js/progress_bar.js
@@ -15,23 +15,47 @@ var ractive = new Ractive({
 function changeProgressBarValue() {
     var e = document.getElementById("progessBarType");
     var strUser = e.options[e.selectedIndex].value;
+    var txt;
+    var numb;
+    var newValue;
     switch (event.target.innerHTML) {
         case"MinusTwentyFive":
             document.getElementById(strUser).value -= 25;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
+            document.getElementById(strUser + "Value").innerHTML = document.getElementById(strUser).value + "%";
             break;
         case"MinusTen":
             document.getElementById(strUser).value -= 10;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
+            document.getElementById(strUser + "Value").innerHTML = document.getElementById(strUser).value + "%";
             break;
         case"PlusTen":
             document.getElementById(strUser).value += 10;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
-            break;
-        case"PlusTwentyFive":
+            if (document.getElementById(strUser).value >= 100) {
+                txt = document.getElementById(strUser + "Value").innerHTML;
+                numb = txt.match(/\d/g).join("");
+                newValue = parseInt(numb);
+                newValue += 10;
+                document.getElementById(strUser + "Value").innerHTML = newValue + "%";
+                break;
+            }
+            else {
+                document.getElementById(strUser + "Value").innerHTML = document.getElementById(strUser).value + "%";
+                break;
+            }
+        case
+        "PlusTwentyFive":
             document.getElementById(strUser).value += 25;
-            document.getElementById(strUser+"Value").innerHTML = document.getElementById(strUser).value + "%";
-            break;
+            if (document.getElementById(strUser).value >= 100) {
+                txt = document.getElementById(strUser + "Value").innerHTML;
+                numb = txt.match(/\d/g).join("");
+                newValue = parseInt(numb);
+                newValue += 25;
+                document.getElementById(strUser + "Value").innerHTML = newValue + "%";
+                break;
+            }
+            else {
+                document.getElementById(strUser + "Value").innerHTML = document.getElementById(strUser).value + "%";
+                break;
+            }
         default :
             break;
     }
